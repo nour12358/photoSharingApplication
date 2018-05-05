01a9dbb03f49438e8752da022f0bbe1babe5cbe5
MahmoudHaouachi <mahmoud.haouachi@gmail.com>
2018-05-05 11:09:32 +0600

    Model properties

--- PhotoSharingApp/Model/Comment.cs
+++ PhotoSharingApp/Model/Comment.cs
@@ -12,5 +12,6 @@ namespace PhotoSharingApp.Model
         public string Subject { get; set; }
         public string Body { get; set; }
         public int PhotoID { get; set; }
+        public virtual Photo Photo { get; set; }
     }
 }
