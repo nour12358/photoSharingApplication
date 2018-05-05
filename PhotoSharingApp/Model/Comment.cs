713bd37ba5be4df7eb518b16accc8ee1a4567772
MahmoudHaouachi <mahmoud.haouachi@gmail.com>
2018-05-05 10:53:47 +0600

    Model properties

--- PhotoSharingApp/Model/Comment.cs
+++ PhotoSharingApp/Model/Comment.cs
@@ -7,5 +7,10 @@ namespace PhotoSharingApp.Model
 {
     public class Comment
     {
+        public int CommentID { get; set; }
+        public string User { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public int PhotoID { get; set; }
     }
 }
--- PhotoSharingApp/Model/Photo.cs
+++ PhotoSharingApp/Model/Photo.cs
@@ -7,5 +7,16 @@ namespace PhotoSharingApp.Model
 {
     public class Photo
     {
+        public int PhotoID { get; set; }
+        public string Title { get; set; }
+        public  byte[] PhotoFile { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
+
+
+
+
     }
+
 }
