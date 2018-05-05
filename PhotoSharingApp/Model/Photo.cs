97fa04c5e2b4ca085d1681d9b3419d0c3e0180fd
MahmoudHaouachi <mahmoud.haouachi@gmail.com>
2018-05-05 11:45:27 +0600

    Adding Controllers

--- PhotoSharingApp/Model/Photo.cs
+++ PhotoSharingApp/Model/Photo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,15 +11,18 @@ namespace PhotoSharingApp.Model
     {
         public int PhotoID { get; set; }
         public string Title { get; set; }
+        [DisplayName("Picture")]
         public  byte[] PhotoFile { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayName("Created Date")]
+        [DisplayFormat(DataFormatString ="{0:MM/dd/yy}",ApplyFormatInEditMode =true)]
         public DateTime CreatedDate { get; set; }
         public string Owner { get; set; }
-        public string Description { get; set; }
         public virtual ICollection<Comment> Comments{ get; set; }
 
-
-
-
+                
     }
 
 }
