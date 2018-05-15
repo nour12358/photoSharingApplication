df2b59150f91dacd60d2ec4df01e1e52e2832ba4
nourjlayel <nour.jlayel.info@gmail.com>
2018-05-15 21:13:08 +0200

    Module4_exercice1

--- PhotoSharingApp/Controllers/PhotoController.cs
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -15,7 +15,72 @@ namespace PhotoSharingApp.Controllers
         public ActionResult Index()
         {
             //var photo = new Photo();
-            return View(context.Photos.First<Photo>());
+            return View(context.Photos.ToList()); 
+                //context.Photos.First<Photo>());
         }
+        public ActionResult Display(int id)
+        {
+            List<Photo> photos = context.Photos.ToList();
+            var verif = photos.Find(photo => photo.PhotoID == id);
+            if (verif != null)
+                return View("display" , verif);
+            else
+                return HttpNotFound();
+        }
+        public ActionResult Create()
+        {
+            Photo photo = new Photo();
+            photo.CreatedDate = DateTime.Now;
+            return View("created", photo);
+        }
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase image)
+        {
+            photo.CreatedDate = DateTime.Now;
+            if (ModelState.IsValid)
+            { if(image!=null)
+                {
+                    photo.ImageMimeType = image.ContentType;
+                    photo.PhotoFile = new byte[image.ContentLength];
+                    image.InputStream.Read(photo.PhotoFile, 0, image.ContentLength);
+                    context.Photos.Add(photo);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("Index");
+            }
+               
+            else
+            {
+                return View("Create", photo); }
+        }
+    
+    public ActionResult Delete(int id)
+    {
+        List<Photo> photos = context.Photos.ToList();
+        var verif = photos.Find(photo => photo.PhotoID == id);
+        if (verif != null)
+            return HttpNotFound();
+        else
+            return View("delete", verif);
+        
     }
+    [HttpPost]
+    [ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        List<Photo> photos = context.Photos.ToList();
+        var verif = photos.Find(photo => photo.PhotoID == id);
+        //verification de lexistence de l'image 
+        //test sur la verif
+
+            context.Entry(verif).State = System.Data.EntityState.Deleted;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+    }
+
+
+    }
+
+
 }
