5b4417bf76a41dff406478ec2aff56566ba97484
nourjlayel <nour.jlayel.info@gmail.com>
2018-06-02 15:19:24 +0200

    réussite des 3 tests task5

--- PhotoSharingTests/PhotoControllerTests.cs
+++ PhotoSharingTests/PhotoControllerTests.cs
@@ -28,7 +28,10 @@ namespace PhotoSharingTests
         [TestMethod]
         public void Test_PhotoGallery_Model_Type()
         {
-            var controller = new PhotoController();
+            var context = new FakePhotoSharingContext(); context.Photos = new[] 
+            { new Photo(), new Photo(), new Photo(), new Photo() }.AsQueryable();
+            var controller = new PhotoController(context);
+            
             var result = controller._PhotoGallery() as PartialViewResult;
             Assert.AreEqual(typeof(List<Photo>), result.Model.GetType());
         }
@@ -36,7 +39,14 @@ namespace PhotoSharingTests
         [TestMethod]
         public void Test_GetImage_Return_Type()
         {
-            var controller = new PhotoController();
+            var context = new FakePhotoSharingContext();
+            context.Photos = new[] 
+            { new Photo { PhotoID = 1, PhotoFile = new byte[1], ImageMimeType = "image/jpeg" },
+              new Photo { PhotoID = 2, PhotoFile = new byte[1], ImageMimeType = "image/jpeg" },
+              new Photo { PhotoID = 3, PhotoFile = new byte[1], ImageMimeType = "image/jpeg" },
+              new Photo { PhotoID = 4, PhotoFile = new byte[1], ImageMimeType = "image/jpeg" }
+            }.AsQueryable();
+            var controller = new PhotoController(context);
             var result = controller.GetImage(1) as ActionResult;
             Assert.AreEqual(typeof(FileContentResult), result.GetType());
         }
